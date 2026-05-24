316629e5f2a19ad64413535045ecb0a988731e64
PGOUP <pgo397454@gmail.com>
2026-05-25 02:34:52 +0800

    修复：插件类型错误 + 添加 react-native-svg 依赖 + tsconfig 路径

--- plugins/build/withClipboardIme.js
+++ plugins/build/withClipboardIme.js
@@ -4,58 +4,63 @@ const config_plugins_1 = require("expo/config-plugins");
 
 const IME_SERVICE_CLASS = 'expo.modules.clipboardime.SyncClipboardImeService';
 
-function addImeService(androidManifest, packageName) {
-    const { manifest } = androidManifest;
-    if (!manifest.application) {
-        manifest.application = [{}];
-    }
-    const application = manifest.application[0];
-    if (!application.service) {
-        application.service = [];
-    }
-    const hasService = application.service.some(
-        (s) => s.$?.['android:name'] === IME_SERVICE_CLASS
-    );
-    if (hasService) {
-        console.log('✓ SyncClipboard IME service already registered');
-        return androidManifest;
-    }
-    application.service.push({
-        $: {
-            'android:name': IME_SERVICE_CLASS,
-            'android:label': 'SyncClipboard IME',
-            'android:permission': 'android.permission.BIND_INPUT_METHOD',
-            'android:exported': 'true',
-        },
-        'intent-filter': [
-            {
-                action: [
+const withClipboardIme = (config) => {
+    return (0, config_plugins_1.withAndroidManifest)(config, (modConfig) => {
+        const androidManifest = modConfig.modResults;
+        const manifest = androidManifest.manifest || {};
+
+        let application = Array.isArray(manifest.application)
+            ? manifest.application[0]
+            : manifest.application;
+        if (!application) {
+            application = {};
+            manifest.application = [application];
+        }
+        if (!Array.isArray(manifest.application)) {
+            manifest.application = [application];
+        }
+
+        if (!application.service) {
+            application.service = [];
+        }
+
+        const hasService = application.service.some(
+            (s) => s?.$?.['android:name'] === IME_SERVICE_CLASS
+        );
+
+        if (!hasService) {
+            application.service.push({
+                $: {
+                    'android:name': IME_SERVICE_CLASS,
+                    'android:label': 'SyncClipboard IME',
+                    'android:permission': 'android.permission.BIND_INPUT_METHOD',
+                    'android:exported': 'true',
+                },
+                'intent-filter': [
+                    {
+                        action: [
+                            {
+                                $: {
+                                    'android:name': 'android.view.InputMethod',
+                                },
+                            },
+                        ],
+                    },
+                ],
+                'meta-data': [
                     {
                         $: {
-                            'android:name': 'android.view.InputMethod',
+                            'android:name': 'android.view.im',
+                            'android:resource': '@xml/ime_method',
                         },
                     },
                 ],
-            },
-        ],
-        'meta-data': [
-            {
-                $: {
-                    'android:name': 'android.view.im',
-                    'android:resource': '@xml/ime_method',
-                },
-            },
-        ],
-    });
-    console.log('✓ Added SyncClipboardImeService to AndroidManifest.xml');
-    return androidManifest;
-}
+            });
+            console.log('✓ Added SyncClipboardImeService to AndroidManifest.xml');
+        } else {
+            console.log('✓ SyncClipboard IME service already registered');
+        }
 
-const withClipboardIme = (config) => {
-    const packageName =
-        config.android?.package ?? 'com.jericx.syncclipboardmobile';
-    return (0, config_plugins_1.withAndroidManifest)(config, (modConfig) => {
-        modConfig.modResults = addImeService(modConfig.modResults, packageName);
         return modConfig;
     });
 };
--- plugins/withClipboardIme.ts
+++ plugins/withClipboardIme.ts
@@ -1,77 +1,68 @@
 import {
-  AndroidConfig,
   ConfigPlugin,
   withAndroidManifest,
   createRunOncePlugin,
 } from 'expo/config-plugins';
 
-const IME_SERVICE_CLASS =
-  'expo.modules.clipboardime.SyncClipboardImeService';
+const IME_SERVICE_CLASS = 'expo.modules.clipboardime.SyncClipboardImeService';
 
-function addImeService(
-  androidManifest: AndroidConfig.Manifest.AndroidManifest,
-  packageName: string
-): AndroidConfig.Manifest.AndroidManifest {
-  const { manifest } = androidManifest;
+const withClipboardIme: ConfigPlugin = (config) => {
+  return withAndroidManifest(config, (modConfig: any) => {
+    const androidManifest = modConfig.modResults;
+    const manifest: any = androidManifest.manifest || {};
 
-  if (!manifest.application) {
-    manifest.application = [{}];
-  }
+    let application: any = Array.isArray(manifest.application)
+      ? manifest.application[0]
+      : manifest.application;
+    if (!application) {
+      application = {};
+      manifest.application = [application];
+    }
+    if (!Array.isArray(manifest.application)) {
+      manifest.application = [application];
+    }
 
-  const application = manifest.application[0];
+    if (!application.service) {
+      application.service = [];
+    }
 
-  if (!application.service) {
-    application.service = [];
-  }
+    const hasService = application.service.some(
+      (s: any) => s?.$?.['android:name'] === IME_SERVICE_CLASS
+    );
 
-  const hasService = application.service.some(
-    (s) => s.$?.['android:name'] === IME_SERVICE_CLASS
-  );
-
-  if (hasService) {
-    console.log('✓ SyncClipboard IME service already registered');
-    return androidManifest;
-  }
-
-  application.service.push({
-    $: {
-      'android:name': IME_SERVICE_CLASS,
-      'android:label': 'SyncClipboard IME',
-      'android:permission': 'android.permission.BIND_INPUT_METHOD',
-      'android:exported': 'true',
-    },
-    'intent-filter': [
-      {
-        action: [
+    if (!hasService) {
+      application.service.push({
+        $: {
+          'android:name': IME_SERVICE_CLASS,
+          'android:label': 'SyncClipboard IME',
+          'android:permission': 'android.permission.BIND_INPUT_METHOD',
+          'android:exported': 'true',
+        },
+        'intent-filter': [
+          {
+            action: [
+              {
+                $: {
+                  'android:name': 'android.view.InputMethod',
+                },
+              },
+            ],
+          },
+        ],
+        'meta-data': [
           {
             $: {
-              'android:name': 'android.view.InputMethod',
+              'android:name': 'android.view.im',
+              'android:resource': '@xml/ime_method',
             },
           },
         ],
-      },
-    ],
-    'meta-data': [
-      {
-        $: {
-          'android:name': 'android.view.im',
-          'android:resource': '@xml/ime_method',
-        },
-      },
-    ],
-  } as NonNullable<(typeof application)['service']>[0]);
+      });
+      console.log('✓ Added SyncClipboardImeService to AndroidManifest.xml');
+    } else {
+      console.log('✓ SyncClipboard IME service already registered');
+    }
 
-  console.log('✓ Added SyncClipboardImeService to AndroidManifest.xml');
-  return androidManifest;
-}
-
-const withClipboardIme: ConfigPlugin = (config) => {
-  const packageName =
-    (config as { android?: { package?: string } }).android?.package ??
-    'com.jericx.syncclipboardmobile';
-
-  return withAndroidManifest(config, (modConfig) => {
-    modConfig.modResults = addImeService(modConfig.modResults, packageName);
     return modConfig;
   });
 };
